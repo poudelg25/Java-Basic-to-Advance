baf6c15ed182e1cdee6627d5b00010b4586bc586
Gopal Poudel <105654693+poudelg25@users.noreply.github.com>
2025-04-09 18:25:02 -0500

    added an example of instance variable scope inside main method

--- variablescope/InstanceVariableExample.java
+++ variablescope/InstanceVariableExample.java
@@ -8,21 +8,21 @@ public class InstanceVariableExample {
         System.out.println(age); // int age is accessible inside constructor
     }
     public static void main(String[] args){
-        new InstanceVariableExample();
+        InstanceVariableExample example = new InstanceVariableExample();
+        //System.out.println(name); // String name is not accessible inside main method directly
+        //System.out.println(age); // int age is not accessible inside main method directly
+        System.out.println(example.name); // String name is accessible inside main method by creating and using an object of the class.
+        System.out.println(example.age); // int age is accessible inside main method by creating and using an object of the class.
     }
-
     public String getName() {
         return name; // String name is accessible inside method
     }
-
     public void setName(String name) {
         this.name = name; // String name is accessible inside method
     }
-
     public int getAge() {
         return age; // int age is accessible inside method
     }
-
     public void setAge(int age) {
         this.age = age; // int age is accessible inside method
     }
